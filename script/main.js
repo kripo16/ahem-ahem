650fda5e32f35c908e2dfbf700bfb529a77278a6
Kripo <117863598+Kripo16@users.noreply.github.com>
2024-06-19 05:38:15 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ alert("ِClick OK then press F11 in the KEYBOARD !!")
 
 document.addEventListener('DOMContentLoaded', function() {
   var currentDate = new Date();
-  var targetDate = new Date('2024-06-20'); // Target date: June 20, 2024
+  var targetDate = new Date('2024-06-19'); // Target date: June 20, 2024
 
   // Check if the current date is the target date
   if (currentDate.toDateString() !== targetDate.toDateString()) {
@@ -12,17 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.style.backgroundPosition = 'center';
     document.body.style.backgroundRepeat = 'no-repeat';
     document.body.style.height = '100%'; // Ensure body covers full viewport height
-    var container = document.querySelector('.container');
-    if (container) {
-      container.style.display = 'none';
-    }
-  } else {
-    // Target date, show content
-    var container = document.querySelector('.container');
-    if (container) {
-      container.style.display = 'block';
-    }
-  }
   
 });
 
