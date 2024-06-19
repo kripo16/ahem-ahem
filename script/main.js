5b172487cac545a417740f1426f4e7516ad6a4c6
Kripo <117863598+Kripo16@users.noreply.github.com>
2024-06-19 04:52:47 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -9,9 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
   // Check if the current date is the target date
   if (currentDate.toDateString() !== targetDate.toDateString()) {
     // Not the target date, change background or display a message
-    document.body.style.backgroundImage = 'url("img/bg.png")'; // Replace with your image URL
+    document.body.style.backgroundImage = 'url("bg.png")'; // Replace with your image URL
     document.body.style.backgroundSize = 'cover';
     document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.height = '100%'; // Ensure body covers full viewport height
   }
 });
 
